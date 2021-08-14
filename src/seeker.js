ec3c38016b4809409ed128e27d7df21d0ba38a64
Simon <4462478+cheesestringer@users.noreply.github.com>
2021-08-14 18:57:27 +1000

    Fix single prices showing undefined

--- src/seeker.js
+++ src/seeker.js
@@ -1,6 +1,9 @@
 chrome.runtime.onMessage.addListener(request => {
   if (request && request.message === 'update') {
     const element = document.querySelector(".property-info-address");
-    element.innerText += `\n${request.price}`;
+    element.innerText += `\n$${request.price[0]}`;
+    if (request.price[1]) {
+      element.innerText += ` to $${request.price[1]}`;
+    };
   }
 });
